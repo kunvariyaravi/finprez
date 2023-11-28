9f4432e175ec9ea94ce389289c5dc59ca695dabb
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-11-29 00:10:11 +0530

    test7

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+     <Image src="/logo.svg" alt="finprez" width={60} height={60} />
+      <Link href="/" className={styles.logo}>FinPrez</Link>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>Homepage</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
