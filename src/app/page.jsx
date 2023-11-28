ed3a917959ccc1f015e41bb0501de00a48305304
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-11-28 23:54:02 +0530

    test4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-// import Featured from "@/components/featured/Featured";
-// import CategoryList from "@/components/categoryList/CategoryList";
-// import CardList from "@/components/cardList/CardList";
-// import Menu from "@/components/Menu/Menu";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
@@ -13,8 +13,8 @@ export default function Home({ searchParams }) {
       {/* <Featured /> */}
       {/* <CategoryList /> */}
       <div className={styles.content}>
-        {/* <CardList page={page}/> */}
-        {/* <Menu /> */}
+        <CardList page={page}/>
+        <Menu />
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-// import AuthLinks from "../authLinks/AuthLinks";
-// import ThemeToggle from "../themeToggle/ThemeToggle";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
   return (
@@ -11,11 +11,11 @@ const Navbar = () => {
      <Image src="/logo.svg" alt="finprez" width={60} height={60} />
       <Link href="/" className={styles.logo}>FinPrez</Link>
       <div className={styles.links}>
-        {/* <ThemeToggle /> */}
+        <ThemeToggle />
         <Link href="/" className={styles.link}>Homepage</Link>
         <Link href="/" className={styles.link}>Contact</Link>
         <Link href="/" className={styles.link}>About</Link>
-        {/* <AuthLinks /> */}
+        <AuthLinks />
       </div>
     </div>
   );
