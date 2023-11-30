36b831af45545ce3b1c82abbc05b6517e6c90c56
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-12-01 00:45:46 +0530

    card

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,4 +1,5 @@
-import CardList from "@/components/cardList/CardList";
+// import CardList from "@/components/cardList/CardList";
+import CardList from "../../components/cardlist/CardList";
 import styles from "./blogPage.module.css";
 import Menu from "@/components/Menu/Menu";
 
