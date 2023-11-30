b4774d0979a136df05af1b232e5c1f9b8bbf6158
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-12-01 00:52:16 +0530

    cardlist

--- src/components/cardlist/CardList.jsx
+++ src/components/cardlist/CardList.jsx
@@ -1,39 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import styles from './cardList.module.css';
-import Pagination from '../pagination/Pagination';
-import Card from '../card/Card';
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+// import Image from "next/image";
+import Card from "../card/Card";
 
-const CardList = ({ page, cat }) => {
-  const [posts, setPosts] = useState([]);
-  const [count, setCount] = useState(0);
+const getData = async (page, cat) => {
+  const res = await fetch(
+    `?page=${page}&cat=${cat || ""}`,
+    {
+      cache: "no-store",
+    }
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://www.finprez.com/api/posts?page=${page}&cat=${cat || ''}`,
-          {
-            cache: 'no-store',
-          }
-        );
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
+  return res.json();
+};
 
-        const data = await response.json();
-        setPosts(data.posts || []);
-        setCount(data.count || 0);
-      } catch (error) {
-        console.error(error);
-        // Handle error (e.g., show an error message to the user)
-      }
-    };
-
-    fetchData();
-  }, [page, cat]);
+const CardList = async ({ page, cat }) => {
+  const { posts, count } = await getData(page, cat);
 
   const POST_PER_PAGE = 2;
+
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
@@ -41,7 +31,7 @@ const CardList = ({ page, cat }) => {
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
-        {posts.map((item) => (
+        {posts?.map((item) => (
           <Card item={item} key={item._id} />
         ))}
       </div>
