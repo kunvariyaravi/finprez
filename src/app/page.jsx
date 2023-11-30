2346444699d9ddb892931e76eed944eb61b4545a
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-12-01 00:49:27 +0530

    card

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
-import CardList from "@/components/cardList/CardList";
+// import CardList from "@/components/cardList/CardList";
+import CardList from "../components/cardlist/CardList";
 import Menu from "@/components/Menu/Menu";
 
 export default function Home({ searchParams }) {
