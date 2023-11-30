7e7f486cd815ca1cb2a72ba81f0feddd6bba02e0
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-12-01 00:19:24 +0530

    cardlist

--- src/components/cardlist/CardList.jsx
+++ src/components/cardlist/CardList.jsx
@@ -1,59 +1,51 @@
-"use Client"
 import React, { useState, useEffect } from 'react';
 import styles from './cardList.module.css';
 import Pagination from '../pagination/Pagination';
 import Card from '../card/Card';
 
 const CardList = ({ page, cat }) => {
-  const [data, setData] = useState({ posts: [], count: 0 });
-  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const getData = async () => {
+    const fetchData = async () => {
       try {
-        const res = await fetch(
+        const response = await fetch(
           `https://www.finprez.com/api/posts?page=${page}&cat=${cat || ''}`,
           {
             cache: 'no-store',
           }
         );
 
-        if (!res.ok) {
-          throw new Error('Failed');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
         }
 
-        const result = await res.json();
-        setData(result);
+        const data = await response.json();
+        setPosts(data.posts || []);
+        setCount(data.count || 0);
       } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
+        console.error(error);
+        // Handle error (e.g., show an error message to the user)
       }
     };
 
-    getData();
+    fetchData();
   }, [page, cat]);
 
   const POST_PER_PAGE = 2;
-
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
-  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < data.count;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <div className={styles.posts}>
-            {data.posts?.map((item) => (
-              <Card item={item} key={item._id} />
-            ))}
-          </div>
-          <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
-        </>
-      )}
+      <div className={styles.posts}>
+        {posts.map((item) => (
+          <Card item={item} key={item._id} />
+        ))}
+      </div>
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
