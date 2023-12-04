cbc1a5cdb08faa11d8c630a7fd54d8d8ca684be5
kunvariyaravi <kunvariyaravi41@gmail.com>
2023-12-04 22:44:29 +0530

    css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
-import CardList from "../components/cardlist/CardList",
+import CardList from "../components/cardlist/CardList";
 import Menu from "@/components/Menu/Menu";
 
 export default function Home({ searchParams }) {
