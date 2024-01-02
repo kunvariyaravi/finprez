f8428ad2aacf488e74c77756aa6bed12dc5908db
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-02 23:00:54 +0530

    description

--- src/app/ipo/[iposlug]/page.jsx
+++ src/app/ipo/[iposlug]/page.jsx
@@ -49,7 +49,7 @@ const ipoPage = async ({ params }) => {
             retail investors is ₹{data.rminamount}.
           </p>
           <h2 className={styles.responsiveHeading}>About {data.company} :-</h2>
-          <div className={styles.responsiveParagraph}>{data.description}</div>
+          <div className={styles.responsiveParagraph} dangerouslySetInnerHTML={{ __html: data?.description }}/>
           <h2 className={styles.responsiveHeading}>{data.title} Details :-</h2>
           <div className={styles.tableContainer}>
             <table className={`${styles.table} ${styles.responsiveTable}`}>
