235b45c5013137309a5a30ccbc3d39dd7a78205c
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-06 23:11:36 +0530

    post metadata set

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,24 +5,22 @@ import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
 import AuthProvider from "@/providers/AuthProvider";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import Head from "next/head";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const lexend = Lexend({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "FinPrez",
+  description: "FinPrez provides the latest stock market, IPO, financial and business news. Get personal finance advice, company news and more",
+  openGraph: {
+    images: '/opengraph-image.jpg',
+  }
+  
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta property="og:title" content="FinPrez" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.finprez.com/" />
-        <meta
-          property="og:image"
-          content="https://www.finprez.com/opengraph-image.jpg"
-        />
-        <meta property="og:description" content="FinPrez provides the latest stock market, IPO, financial and business news. Get personal finance advice, company news and more" />
-      </head>
       <body className={lexend.className}>
         <AuthProvider>
           <ThemeContextProvider>
--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Menu from "@/components/Menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
-import Head from 'next/head';
+import Head from "next/head";
 
 const getData = async (slug) => {
   const res = await fetch(`https://www.finprez.com/api/posts/${slug}`, {
@@ -18,53 +18,85 @@ const getData = async (slug) => {
   return res.json();
 };
 
+export async function generateStaticParams() {
+  try {
+    const response = await fetch("https://www.finprez.com/api/posts");
+    const { posts } = await response.json();
+
+    // Check if 'posts' is an array before mapping over it
+    if (Array.isArray(posts)) {
+      return posts.map(({ id }) => ({ params: { slug: id.toString() } }));
+    } else {
+      console.error(
+        "API response did not contain a valid 'posts' array:",
+        posts
+      );
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
+}
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  const response = await fetch(`https://www.finprez.com/api/posts/${slug}`);
+  const post = await response.json();
+
+  return {
+    title: post.title,
+    description: post.desc.substring(0, 160),
+    openGraph: {
+      images: [
+        {
+          url: post.img,
+        },
+      ],
+    },
+  };
+}
+
 const SinglePage = async ({ params }) => {
   const { slug } = params;
 
   const data = await getData(slug);
 
-  // Set dynamic metadata
-  const pageTitle = data?.title;
-  const pageDescription = data?.description; // replace with the actual property in your data
-  const pageImage=data?.img;
-
   return (
-
     <div className={styles.container}>
-      <Head>
+      {/* <Head>
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
         <meta property="og:image" content={pageImage} />
-      </Head>
+      </Head> */}
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{data?.title}</h1>
-        <div className={styles.user}>
-          {data?.user?.image && (
-            <div className={styles.userImageContainer}>
-              <Image
-                src={data.user.image}
-                alt=""
-                fill
-                className={styles.avatar}
-              />
-            </div>
-          )}
-          <div className={styles.userTextContainer}>
-            <span className={styles.username}>{data?.user.name}</span>
-            <span className={styles.date}>
-              Published on {data?.createdAt.substring(0, 10)}
-            </span>
-          </div>
-        </div>
       </div>
       {data?.img && (
         <div className={styles.imageContainer}>
           <Image src={data.img} alt="" fill className={styles.image} />
         </div>
       )}
-
+      <div className={styles.user}>
+        {data?.user?.image && (
+          <div className={styles.userImageContainer}>
+            <Image
+              src={data.user.image}
+              alt=""
+              fill
+              className={styles.avatar}
+            />
+          </div>
+        )}
+        <div className={styles.userTextContainer}>
+          <span className={styles.username}>{data?.user.name}</span>
+          <span className={styles.date}>
+            Published on {data?.createdAt.substring(0, 10)}
+          </span>
+        </div>
+      </div>
       <div className={styles.content}>
         <div className={styles.post}>
           <div
