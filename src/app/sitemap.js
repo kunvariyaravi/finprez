3e58b833a6f9b2d029753544987b84e0aa8558a8
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-08 00:16:16 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -17,11 +17,11 @@ export default async function sitemap() {
         url: `https://www.finprez.com/ipo/${iposlug}`,
       }));
   
-      // Build the sitemap XML content
+      // Build the sitemap XML content with proper indentation
       const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          ${ipopostEntries.map(entry => `<url><loc>${entry.url}</loc></url>`).join('\n')}
-        </urlset>`;
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${ipopostEntries.map(entry => `  <url>\n    <loc>${entry.url}</loc>\n  </url>`).join('\n')}
+  </urlset>`;
   
       return sitemapXML;
     } catch (error) {
