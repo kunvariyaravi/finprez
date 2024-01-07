23079240dc9605e89df8f6fb40a830523bb03641
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-07 23:36:31 +0530

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default async function sitemap() {
+    const response = await fetch(`https://www.finprez.com/api/ipopost/${iposlug}`);
+    const { posts } = await response.json();
+  
+    const ipopostEntries = posts.map(({ iposlug }) => ({
+      url: `https://www.finprez.com/ipopost/${iposlug}`,
+      // lastModified: new Date(post.updatedAt),
+      // changeFrequency:,
+      // priority:
+    }));
+  
+    return [
+    //   {
+    //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+    //     lastModified: new Date(),
+    //   },
+      ...postEntries,
+    ];
+  }
+  
