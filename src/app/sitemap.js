d2e64ec30f3098629d6ee65ff8127d8426f6c663
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-08 00:13:05 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,54 +1,32 @@
 export default async function sitemap() {
-  try {
-    const response = await fetch("https://www.finprez.com/api/ipopost", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    console.log("API Response:", response);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    try {
+      const response = await fetch("https://www.finprez.com/api/ipopost");
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      }
+  
+      const { posts } = await response.json();
+  
+      if (!posts || posts.length === 0) {
+        console.error("No posts found in the API response.");
+        return ''; // Return an empty string or handle accordingly if no posts are found.
+      }
+  
+      const ipopostEntries = posts.map(({ iposlug }) => ({
+        url: `https://www.finprez.com/ipo/${iposlug}`,
+      }));
+  
+      // Build the sitemap XML content
+      const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${ipopostEntries.map(entry => `<url><loc>${entry.url}</loc></url>`).join('\n')}
+        </urlset>`;
+  
+      return sitemapXML;
+    } catch (error) {
+      console.error("Error:", error);
+      return ''; // Return an empty string or handle accordingly in case of an error.
     }
-
-    const { posts } = await response.json({
-        xml: {
-           attributeNamePrefix: '',
-           ignoreAttributes: false,
-           parseNodeValue: false,
-           trimValues: true,
-           cdataTagName: '__cdata',
-           cdataPositionChar: '\\c',
-           locale: 'en',
-           normalizeTags: false,
-           tagValueProcessor: a => he.decode(a, {isAttributeValue: true}),
-           attrValueProcessor: a => he.decode(a, {isAttributeValue: true})
-        }
-       });
-
-    if (!posts || posts.length === 0) {
-      console.error("No posts found in the API response.");
-      return [];
-    }
-
-    const ipopostEntries = posts.map(({ iposlug }) => ({
-      url: `https://www.finprez.com/ipo/${iposlug}`,
-      // lastModified: new Date(post.updatedAt),
-      // changeFrequency:,
-      // priority:
-    }));
-
-    console.log("IPO Post Entries:", ipopostEntries);
-
-    return [
-      //   {
-      //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
-      //     lastModified: new Date(),
-      //   },
-      ...ipopostEntries,
-    ];
-  } catch (error) {
-    console.error("Error:", error.message);
-    return [];
   }
-}
+  
