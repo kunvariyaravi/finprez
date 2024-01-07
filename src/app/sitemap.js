f2d45bd35437a09a3ed97db0578c420c28ed57d1
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-07 23:54:43 +0530

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,35 +1,38 @@
 export default async function sitemap() {
-    try {
-      const response = await fetch("https://www.finprez.com/api/ipopost");
-      
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data. Status: ${response.status}`);
-      }
-  
-      const { posts } = await response.json();
-  
-      if (!posts || posts.length === 0) {
-        console.error("No posts found in the API response.");
-        return [];
-      }
-  
-      const ipopostEntries = posts.map(({ iposlug }) => ({
+  try {
+    const response = await fetch("https://www.finprez.com/api/ipopost");
+    console.log("API Response:", response);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+
+    const { posts } = await response.json();
+
+    if (!posts || posts.length === 0) {
+      console.error("No posts found in the API response.");
+      return [];
+    }
+
+    const ipopostEntries = posts.map(({ iposlug }) => ({
         url: `https://www.finprez.com/ipo/${iposlug}`,
         // lastModified: new Date(post.updatedAt),
         // changeFrequency:,
         // priority:
       }));
-  
-      return [
-        //   {
-        //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
-        //     lastModified: new Date(),
-        //   },
-        ...ipopostEntries,
-      ];
-    } catch (error) {
-      console.error("Error:", error.message);
-      return [];
-    }
+      
+      console.log("IPO Post Entries:", ipopostEntries);
+      
+
+    return [
+      //   {
+      //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+      //     lastModified: new Date(),
+      //   },
+      ...ipopostEntries,
+    ];
+  } catch (error) {
+    console.error("Error:", error.message);
+    return [];
   }
-  
+}
