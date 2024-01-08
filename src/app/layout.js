74ca029654042fd91ba3711baa6396231ca27252
kunvariyaravi <kunvariyaravi41@gmail.com>
2024-01-08 16:13:49 +0530

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
                 <div className="wrapper">
                   <Navbar />
                   {children}
+                  <Analytics />
                   <SpeedInsights />
                   <Footer />
                 </div>
